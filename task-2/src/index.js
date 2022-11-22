671aa38b69c793adc9d7867370ddeb031b1ede5e
Mikhail Varushichev <mikeloangel@gmail.com>
2022-11-23 00:10:03 +0100

    fix: basic FUOC fix

--- task-2/src/index.js
+++ task-2/src/index.js
@@ -6,4 +6,11 @@ function handleCallToAction(){
   alert('Call me to action!')
 }
 
+// easy FUOC fix (see index.html style tag)
+function handleFuoc(){
+  let body = document.querySelector('body');
+  body.style.visibility = 'visible';
+}
+
 callToActionBtn.addEventListener('click', handleCallToAction);
+window.addEventListener('load', handleFuoc)
