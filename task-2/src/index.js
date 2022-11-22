891c0a29c02530865da9e98c0e3e111d6e17e646
Mikhail Varushichev <mikeloangel@gmail.com>
2022-11-22 22:49:46 +0100

    fix: makes code prettier and some comments added

--- task-2/src/index.js
+++ task-2/src/index.js
@@ -1,1 +1,9 @@
 import './index.scss';
+
+const callToActionBtn = document.querySelector('.main__aside-button');
+
+function handleCallToAction(){
+  alert('Call me to action!')
+}
+
+callToActionBtn.addEventListener('click', handleCallToAction);
